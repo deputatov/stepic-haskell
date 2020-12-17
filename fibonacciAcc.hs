933f007ad4ea602f502fd879c6047444933f0291
Nikita Deputatov <deputatovn@gmail.com>
2020-12-17 17:03:12 +0300

    Add solutions

---
+++ fibonacciAcc.hs
@@ -0,0 +1,5 @@
+fibonacci n = helper (0, 1) n where
+    helper (r2, r1) n | n == 0 = r2
+                      | n == 1 = r1
+                      | n > 1  = helper (r1, r2 + r1) (n - 1) 
+                      | n < 0  = helper (r1 - r2, r2) (n + 1) 
